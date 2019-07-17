aac3a48962023813dfaa4d7a704344fb807a787f
acj9189 <acj8991@gmail.com>
2019-07-17 13:20:47 -0500

    Actu

--- FontedNPM/app/User.php
+++ FontedNPM/app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasRoles(array $roles) {
+      return in_array($this->role, $roles);
+   }
+
 }
