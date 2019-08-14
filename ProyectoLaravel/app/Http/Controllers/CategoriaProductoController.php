1ed41960180c5fb0e97c8c3d7fabaa6c228497c7
acj9189 <acj8991@gmail.com>
2019-08-14 13:35:58 -0500

    Actualizaciones proyecto

---
+++ ProyectoLaravel/app/Http/Controllers/CategoriaProductoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Categoria_Producto;
+use Illuminate\Http\Request;
+
+class CategoriaProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Categoria_Producto  $categoria_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categoria_Producto $categoria_Producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Categoria_Producto  $categoria_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categoria_Producto $categoria_Producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Categoria_Producto  $categoria_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categoria_Producto $categoria_Producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Categoria_Producto  $categoria_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categoria_Producto $categoria_Producto)
+    {
+        //
+    }
+}
---
+++ ProyectoLaravel/app/Http/Controllers/PresentacionProductoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Presentacion_Producto;
+use Illuminate\Http\Request;
+
+class PresentacionProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Presentacion_Producto  $presentacion_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Presentacion_Producto $presentacion_Producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Presentacion_Producto  $presentacion_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Presentacion_Producto $presentacion_Producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Presentacion_Producto  $presentacion_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Presentacion_Producto $presentacion_Producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Presentacion_Producto  $presentacion_Producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Presentacion_Producto $presentacion_Producto)
+    {
+        //
+    }
+}
